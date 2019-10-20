3faa87b33f418cf6141d16e96abe9ad9b279f97c
Julie Yohn <julie.yohn@student.swic.edu>
2019-10-20 15:58:33 -0500

    Add pet calculator with style sheet.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,27 @@
-// Enter a description of this program here.
+// Simple arithmetic pet calculator app.
 
-/**
- * Write some pseudo code steps here.
- * Use those steps as your `commit` messages (and delete them from here as you complete a step.)
- */
+const calculator = {
+  multiply1(a) {
+    return a * 7 + " years old in dog years.";
+  },
+  multiply2(a) {
+    return a / 7 + " years old in human years.";
+  }
+};
+
+const form = document.querySelector("form");
+const name1 = document.querySelector("#name-1");
+const num1 = document.querySelector("#num-1");
+const select = document.querySelector("select");
+const output = document.querySelector("output");
+
+//Use an anonymous callback function as the 2nd argument.
+form.addEventListener("submit", function(event) {
+  //Prevent the default browser behavior - DON'T submit
+  event.preventDefault();
+
+  //form inputs have a property called value.
+
+  output.textContent =
+    name1.value + " is " + calculator[select.value](num1.value);
+});
